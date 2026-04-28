fdfb980673333d3ea42bc08fe50047460a8fb49c
EvanThomas2987 <evan.thomas@mail.bcu.ac.uk>
2026-04-28 10:25:24 +0100

    Changed some weapon sounds

--- ProjectInside/Assets/Creator Kit - FPS/Scripts/KeyLockSystem/Key.cs
+++ ProjectInside/Assets/Creator Kit - FPS/Scripts/KeyLockSystem/Key.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using FMOD.Studio;
+using FMODUnity;
 
 public class Key : MonoBehaviour
 {
     public string keyType;
     public Text KeyNameText;
+
+    [Header("Audio")]
+    public AudioClip KeyCollectAudioClip;
+    [SerializeField] public EventReference KeyCollectEvent;
 
     void OnEnable()
     {
